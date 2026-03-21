a9932f9ae6f7ffb1a1d01ae44cabbe132b4ad018
Samuelyojo <samijoo222@gmail.com>
2026-03-21 16:12:49 +0300

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+class Main1 {
+    public static void main(String[] args) {
+//         System.out.print("Number :");
+//         // int number = scanner.nextInt();
+
+//         //  if (number % 5 == 0 && number % 3 == 0) {
+//         //     System.out.println("FiZZBUZZ");
+//         //  } else if (number % 3 == 0){
+//         //      System.out.println("BUZZ");
+//         //  }else if (number % 5 == 0) 
+//         //      System.out.println("FiZZ");
+//         // else 
+//         //      System.out.println(number);
+
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("Full name :");
+        // String fullName = scanner.nextLine();
+        // System.out.print("Age :");
+        // byte age = scanner.nextByte();
+        // System.out.print("Password :");
+        // String password = scanner.next();
+
+        // System.out.println("Your name is :" + fullName);
+        // System.out.println("Your age is :" + age);
+        // System.out.println("Your password is :" + password);
+
+    }
+}
